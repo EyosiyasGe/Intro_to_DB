649cd717d00929174a10f15ecdf25fa0a24d0db9
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-29 19:42:51 +0300

    DB INTRO

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,7 +5,7 @@ USE alx_book_store;
 
 CREATE TABLE IF NOT EXISTS Authors (author_id INT AUTO_INCREMENT PRIMARY KEY , author_name VARCHAR(215) NOT NULL);
 
-CREATE TABLE IF NOT EXISTS Books(book_id INT AUTO_INCREMENT PRIMARY KEY, tittle VARCHAR(130) NOT NULL, price DOUBLE NOT NULL, publication_date DATE , FOREIGN KEY (author_id) REFERENCES Authors(author_id) );
+CREATE TABLE IF NOT EXISTS Books(book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130) NOT NULL, price DOUBLE NOT NULL, publication_date DATE , FOREIGN KEY (author_id) REFERENCES Authors(author_id) );
 
 CREATE TABLE IF NOT EXISTS Customers ( customer_id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(215) NOT NULL, email VARCHAR(215) UNIQUE NOT NULL, address TEXT  ) ;
 
