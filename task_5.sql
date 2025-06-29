be225f906016e255add3defdd453907b5e9534b0
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-29 20:19:27 +0300

    DB INTRO

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+USE alx_book_store;
+INSERT INTO customers (customer_id,customer_name,email, address ) Values (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+mysql -u root -p alx_book_store < task_5.sql
--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,3 @@
-INSERT INTO customers (customer_id,customer_name,email, address ) Values (2,'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),(3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave.')(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.')
+USE alx_book_store;
+INSERT INTO customers (customer_id,customer_name,email, address ) Values (2,'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),(3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness  Ave.')(4,'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+mysql -u root -p alx_book_store < task_6.sql
